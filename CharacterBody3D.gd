293b70bb9d07efe96e70feec7999430cd643b09a
Pedro <pedrosteinmacher@gmail.com>
2024-08-10 14:17:40 -0300

    noah cara de bunda

---
+++ CharacterBody3D.gd
@@ -0,0 +1,18 @@
+extends CharacterBody3D
+
+
+const SPEED = 500.0
+const JUMP_VELOCITY = 4.5
+
+
+func _physics_process(delta):
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
