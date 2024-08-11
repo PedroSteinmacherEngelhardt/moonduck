b952173dd477ddfd33ed4aea6d2bd863313f14f0
Behbs <ben.s.bussmann@gmail.com>
2024-08-11 20:57:48 -0300

    "isometrico"

--- tower_defense/TestMess/Camera3D.gd
+++ tower_defense/TestMess/Camera3D.gd
@@ -1,2 +1,7 @@
 extends Camera3D
 
+
+
+func _process(delta):
+	if Input.is_action_just_released("camera_turn_right"):
+		print("yay")
--- tower_defense/TestMess/camera/CharacterBody3D.gd
+++ tower_defense/TestMess/camera/CharacterBody3D.gd
@@ -4,7 +4,7 @@ const spd = 5.0
 
 func _physics_process(delta):
 
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir = Input.get_vector("camera_left", "camera_right", "camera_foward", "camera_backward")
 	var dir = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if dir:
 		velocity.x = dir.x * spd
