6a89038a06d6199613bd99feaf8aa152b4853030
bruno.lopes <95176651+koddoki@users.noreply.github.com>
2024-08-17 15:10:37 -0300

    enemy death end goal animation

--- tower_defense/TestMess/purple_enemie.gd
+++ tower_defense/TestMess/purple_enemie.gd
@@ -4,6 +4,16 @@ extends CharacterBody3D
 @export var speed = 5.0
 @export var target : Vector3
 
+@onready var animation_player : = $AnimationPlayer
+
 
 func _physics_process(delta: float) -> void:
 	position = position.move_toward(target, delta * speed)
+
+
+func fly_away() -> void:
+	animation_player.play("byebye")
+
+
+func morri() -> void:
+	animation_player.play("morri")
